7ccfc43f06b9505b9467fd446d8c32caf6ebf7ce
Tobi Adeyemi <Jeezman@users.noreply.github.com>
2023-03-15 20:47:17 +0100

    Update HomeCollection.tsx

--- auction-ui/src/components/HomeCollection.tsx
+++ auction-ui/src/components/HomeCollection.tsx
@@ -11,7 +11,7 @@ const HomeCollection = ({ products }: Props) => {
     <section className="flex w-full flex-col items-center justify-center py-40">
       <h1 className="text-center text-7xl text-primary">Upcoming Auctions</h1>
       <div className="mt-20 grid w-full grid-cols-1 gap-6 md:w-[52%] md:grid-cols-2">
-        {products?.map((product, idx) => (
+        {products.map((product, idx) => (
           <CollectionProductBlock key={idx} product={product} />
         ))}
       </div>
