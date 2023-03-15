0c20494fd6e39b34016a2c6dbc9949a2690ff72e
Jeezman <tobiadeyemi91@gmail.com>
2023-03-15 18:16:18 +0100

    fix

--- auction-ui/src/api/auction/getFeaturedAuctions.ts
+++ auction-ui/src/api/auction/getFeaturedAuctions.ts
@@ -1,8 +1,8 @@
 import { makeServerRequest } from 'src/api/serverRequest'
 import { Products } from 'src/api/auction/types'
 
-export async function getFeaturedAuctions(): Promise<Products> {
-  const featuredAuction: Products = await makeServerRequest({
+export async function getFeaturedAuctions(): Promise<Products[]> {
+  const featuredAuction: Products[] = await makeServerRequest({
     method: 'GET',
     path: `api/auctions/featured`,
   })
