d24a406d90de2ced2933c80baf91a673c6fd1095
Jeezman <tobiadeyemi91@gmail.com>
2023-03-16 17:14:35 +0100

    fix

--- auction-ui/app/layout.tsx
+++ auction-ui/app/layout.tsx
@@ -4,7 +4,6 @@ import 'react-medium-image-zoom/dist/styles.css'
 import { PropsWithChildren } from 'react'
 import Footer from 'src/components/shared/Footer'
 import Header from 'src/components/shared/Header'
-import Link from 'src/components/shared/Link'
 
 export default async function RootLayout(props: PropsWithChildren<unknown>) {
   return (
@@ -13,7 +12,6 @@ export default async function RootLayout(props: PropsWithChildren<unknown>) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
-        <Link href="/test">test</Link>
         <section className="flex h-screen w-screen flex-col justify-between">
           <Header />
           <div className="bg-white">{props.children}</div>
