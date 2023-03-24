8b91d1d8d17cd08f730c879fd1cf33b473e4478d
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-24 13:26:56 +0100

    feat: payment flow fixes

--- auction-ui/src/components/pages/home/AuctionOfTheDay/index.tsx
+++ auction-ui/src/components/pages/home/AuctionOfTheDay/index.tsx
@@ -20,12 +20,11 @@ export default function AuctionOfTheDay({ auction: auctionResponse }: AuctionOfT
 
   const { auction } = auctionResponse
 
-  const start_iso = parseISO(auction.start_at)
-  const end_iso = parseISO(auction.end_at)
-  console.log(start_iso, end_iso)
+  const start = parseISO(auction.start_at)
+  const end = parseISO(auction.end_at)
 
-  const start = format(start_iso, 'do MMMM, yyyy hh:mm aaa')
-  const end = format(end_iso, 'do MMMM, yyyy hh:mm aaa')
+  // const start = format(start_iso, 'do MMMM, yyyy hh:mm aaa')
+  // const end = format(end_iso, 'do MMMM, yyyy hh:mm aaa')
 
   return (
     <section className="mt-28 flex w-full items-center justify-center px-5 md:px-0">
