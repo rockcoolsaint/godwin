6ab3f810785c857d3ca3599d91d2086209aee6ba
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-24 13:08:39 +0100

    feat: payment flow fixes

--- auction-ui/src/components/pages/home/AuctionOfTheDay/index.tsx
+++ auction-ui/src/components/pages/home/AuctionOfTheDay/index.tsx
@@ -20,8 +20,8 @@ export default function AuctionOfTheDay({ auction: auctionResponse }: AuctionOfT
 
   const { auction } = auctionResponse
 
-  const start = format(parseISO(auction.start_at), 'do MMMM, yyyy hh:mm aaa')
-  // const end = format(parseISO(auction.end_at), 'do MMMM, yyyy hh:mm aaa')
+  // const start = format(parseISO(auction.start_at), 'do MMMM, yyyy hh:mm aaa')
+  const end = format(parseISO(auction.end_at), 'do MMMM, yyyy hh:mm aaa')
 
   return (
     <section className="mt-28 flex w-full items-center justify-center px-5 md:px-0">
@@ -35,11 +35,11 @@ export default function AuctionOfTheDay({ auction: auctionResponse }: AuctionOfT
             <div className="flex justify-between">
               <aside>
                 <p className="text-base text-dark-100">{t('home.bid_start_date')}:</p>
-                <p className="text-base font-semibold">{start}</p>
+                {/* <p className="text-base font-semibold">{start}</p> */}
               </aside>
               <aside className="md:ml-9">
                 <p className="text-base text-dark-100">{t('home.bid_end_date')}:</p>
-                {/* <p className="text-base font-semibold">{end}</p> */}
+                <p className="text-base font-semibold">{end}</p>
               </aside>
             </div>
             <hr className="my-5 block w-full border" />
