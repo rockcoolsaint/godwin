3d550e954b26c59886a1036443d237bc1c06f25e
Jeezman <tobiadeyemi91@gmail.com>
2023-03-29 14:58:10 +0100

    clean up

--- auction-ui/src/components/pages/auction/AuctionProfile/index.tsx
+++ auction-ui/src/components/pages/auction/AuctionProfile/index.tsx
@@ -1,20 +1,24 @@
 'use client'
 import Image from 'next/image'
 import Link from 'src/components/shared/Link'
+import { useTranslation } from 'src/hooks'
 import { Auction } from 'src/types'
+import { formatDate } from 'src/utils/date'
 
 interface Props {
   data: Auction
 }
 
 const AuctionProfile = ({ data }: Props) => {
+  const { t } = useTranslation()
+
   return (
     <section className="rounded-3 flow-root rounded-xl border bg-gray-50  px-4 py-3">
       <div className="border-1 flex justify-between rounded-xl bg-white p-2">
         <div className="flex-1 ">
           <div className="flex items-center rounded-t-xl border-b-2 border-white  odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Name</p>
+              <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.name')}</p>
             </aside>
             <aside>
               <p className="p-3 py-4 pl-4 text-sm font-semibold">{data.title}</p>
@@ -22,7 +26,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Hashrate</p>
+              <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.hashrate')}</p>
             </aside>
             <aside>
               <p className="p-3 py-4 pl-4">{data.auction_meta.hashrate}</p>
@@ -30,7 +34,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Location</p>
+              <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.location')}</p>
             </aside>
             <aside>
               <p className="p-3 py-4  pl-4">{data.auction_meta.location}</p>
@@ -38,7 +42,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Days of mining</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.days_of_mining')}</p>
             </aside>
             <aside>
               <p className="p-3 pl-4">{data.auction_meta.days_of_mining}</p>
@@ -46,7 +50,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Hours</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.hours')}</p>
             </aside>
             <aside>
               <p className="p-3 pl-4">{data.auction_meta.hours_per_day}</p>
@@ -54,7 +58,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Power source</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.power_source')}</p>
             </aside>
             <aside>
               <p className="p-3 py-4 pl-4">{data.auction_meta.power_source}</p>
@@ -62,7 +66,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">ASIC model</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.asic_model')}</p>
             </aside>
             <aside>
               <p className="p-3 pl-4">{data.auction_meta.asic_model}</p>
@@ -70,7 +74,7 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Terms</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.auction_terms')}</p>
             </aside>
             <aside>
               <Link href={data?.auction_meta.terms_link} className="p-3 pl-4">
@@ -80,18 +84,18 @@ const AuctionProfile = ({ data }: Props) => {
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Auction start</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.auction_start')}</p>
             </aside>
             <aside>
-              <p className="p-3 pl-4">{new Date(data.start_at).toLocaleString()}</p>
+              <p className="p-3 pl-4">{formatDate(data.start_at)}</p>
             </aside>
           </div>
           <div className="flex items-center rounded-b-xl border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
             <aside className=" w-1/4 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-1/6">
-              <p className="p-3 py-4 font-semibold text-dark-100">Auction end</p>
+              <p className="p-3 py-4 font-semibold text-dark-100">{t('home.auction_end')}</p>
             </aside>
             <aside>
-              <p className="p-3 pl-4">{new Date(data.end_at).toLocaleString()}</p>
+              <p className="p-3 pl-4">{formatDate(data.end_at)}</p>
             </aside>
           </div>
         </div>
