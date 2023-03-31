dad680cd3245b3c1948493cc4dea4968d27fa806
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-03-31 14:03:37 +0200

    feat: initial setup websockets

--- auction-ui/src/lib/ws.ts
+++ auction-ui/src/lib/ws.ts
@@ -17,7 +17,7 @@ class WS {
 
   connect(accountId: number, token: string): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.socket = new WebSocket('ws://localhost:8000/ws/socket_server/')
+      this.socket = new WebSocket(`${process.env.NEXT_PUBLIC_APP_WS_SERVER_URL}/ws/socket_server/`)
       this.socket.onerror = reject
       this.socket.onopen = () => {
         this.emit('authorize', { account_id: accountId, token })
