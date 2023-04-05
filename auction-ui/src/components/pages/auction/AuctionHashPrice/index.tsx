202aab713d39116201bfcb1fe0f192e661c6e226
Jeezman <tobiadeyemi91@gmail.com>
2023-04-05 17:54:14 +0100

    add plot data

--- auction-ui/src/components/pages/auction/AuctionHashPrice/index.tsx
+++ auction-ui/src/components/pages/auction/AuctionHashPrice/index.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import hashPrice from 'src/assets/png/hash_price.png'
+import Plot from 'react-plotly.js'
+import data from 'src/assets/json/auction_live_feed.json'
+import { PlotDataType } from 'src/components/pages/auction/types'
 
 export default function AuctionHashPrice() {
+  const plot = data as PlotDataType
+
   return (
-    <div className="flex h-full items-center justify-center">
-      <Image className="w-3/4 object-cover" alt="Auction hash price" src={hashPrice} width={500} height={500} />
+    <div className="flex h-full items-center justify-center overflow-scroll">
+      <Plot data={plot.data} layout={plot.layout} />
     </div>
   )
 }
--- auction-ui/src/components/pages/auction/AuctionLiveFeed/index.tsx
+++ auction-ui/src/components/pages/auction/AuctionLiveFeed/index.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import livefeed from 'src/assets/png/live_feed.png'
+import Plot from 'react-plotly.js'
+import data from 'src/assets/json/auction_live_feed.json'
+import { PlotDataType } from 'src/components/pages/auction/types'
 
 export default function AuctionLiveFeed() {
+  const plot = data as PlotDataType
+
   return (
-    <div className="flex h-full items-center justify-center">
-      <Image className="w-3/4 object-cover" alt="Auction live feed" src={livefeed} width={500} height={500} />
+    <div className="flex h-full items-center justify-center overflow-scroll">
+      <Plot data={plot.data} layout={plot.layout} />
     </div>
   )
 }
