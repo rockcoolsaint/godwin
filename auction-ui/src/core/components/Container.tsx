457c014a9cd0c34bd7768afd0928fa78a1f92f03
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-04-12 13:57:50 +0200

    chore: styling fixes

--- auction-ui/src/core/components/Container.tsx
+++ auction-ui/src/core/components/Container.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
 
 export default function Container({ className, children }: { className?: string; children?: React.ReactNode }) {
-  return <div className={clsx(className, 'mx-auto w-full px-4 md:w-4/5 md:px-0 2xl:w-3/5')}>{children}</div>
+  return <div className={clsx(className, 'mx-auto w-full px-5 md:w-4/5 md:px-0')}>{children}</div>
 }
