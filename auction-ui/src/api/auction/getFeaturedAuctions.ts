fa50098ad762b5495ce625ce420d48c4a8fd3fbe
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-04-18 17:24:26 +0200

    chore: limit /featured to 6 items

--- auction-ui/src/api/auction/getFeaturedAuctions.ts
+++ auction-ui/src/api/auction/getFeaturedAuctions.ts
@@ -6,7 +6,7 @@ import { Auction } from 'src/api/auction/types'
 export async function getFeaturedAuctions(): Promise<Auction[]> {
   const featuredAuction: Auction[] = await makeClientRequest({
     method: 'GET',
-    path: `/api/auctions/featured`,
+    path: `/api/auctions/featured?limit=6`,
   })
 
   return featuredAuction
