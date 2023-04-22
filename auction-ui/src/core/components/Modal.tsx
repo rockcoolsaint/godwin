201740bb8b59d553e982ad89a399c6553dd2f2ae
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-04-22 20:48:30 +0200

    chore: cleanup

--- auction-ui/src/core/components/Modal.tsx
+++ auction-ui/src/core/components/Modal.tsx
@@ -5,7 +5,6 @@
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import React, { useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
 import { Toast } from 'src/core'
 import Link from 'src/components/shared/Link'
 
@@ -13,36 +12,35 @@ function MyVerticallyCenteredModal(props: any) {
   // const [maxValue, setMaxValue] = useState('');
   const [toastData, setToastData] = useState({ message: '' })
   const [showToast, setShowToast] = useState(false)
-  const { getIdTokenClaims } = useAuth0()
   const [query1, setQuery1] = useState('')
 
   const handleSubmit1 = () => {
-    getIdTokenClaims().then(async (data1: any) => {
-      if (!query1) return
-      if (!data1?.__raw) return
-      fetch('/api/place-automatic-bid/', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + data1?.__raw },
-        body: JSON.stringify({
-          proxy_bid_amnt: query1,
-          source: 'list_page',
-          list_id: props.data,
-        }),
-      })
-        .then(response => response.json())
-        .then(responseJson => {
-          props.onHide()
-          setToastData({
-            message: responseJson.place_bid_status.status,
-          })
-          setShowToast(true)
-
-          return responseJson
-        })
-        .catch(error => {
-          console.error(error)
-        })
-    })
+    // TODO: Refactor using new auth system
+    // getIdTokenClaims().then(async (data1: any) => {
+    //   if (!query1) return
+    //   if (!data1?.__raw) return
+    //   fetch('/api/place-automatic-bid/', {
+    //     method: 'post',
+    //     headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + data1?.__raw },
+    //     body: JSON.stringify({
+    //       proxy_bid_amnt: query1,
+    //       source: 'list_page',
+    //       list_id: props.data,
+    //     }),
+    //   })
+    //     .then(response => response.json())
+    //     .then(responseJson => {
+    //       props.onHide()
+    //       setToastData({
+    //         message: responseJson.place_bid_status.status,
+    //       })
+    //       setShowToast(true)
+    //       return responseJson
+    //     })
+    //     .catch(error => {
+    //       console.error(error)
+    //     })
+    // })
   }
 
   const handleChange4 = (e: React.ChangeEvent<HTMLInputElement>) => {
--- auction-ui/src/hooks/useSatsToFiat.ts
+++ auction-ui/src/hooks/useSatsToFiat.ts
@@ -1,22 +1,30 @@
+'use client'
+
 import { satoshisToFiat } from 'bitcoin-conversion'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BidsEntityOrCurrentBid } from 'src/api/auction/types'
 
 export default function useSatsToFiat({ initialValue = 0, bid }: { initialValue: number; bid: BidsEntityOrCurrentBid | number }) {
   const [priceInFiat, setPriceInFiat] = useState(initialValue)
+
+  useEffect(() => {
+    const getPrice = async () => {
+      let price
+      if (typeof bid == 'number') {
+        price = await satoshisToFiat(bid, 'USD')
+      } else {
+        price = await satoshisToFiat(bid.bid, 'USD')
+      }
+
+      setPriceInFiat(price)
+    }
+
+    getPrice()
+  }, [bid])
+
   if (!Boolean(bid)) {
     return 0
   }
-  ;(async () => {
-    let price
-    if (typeof bid == 'number') {
-      price = await satoshisToFiat(bid, 'USD')
-    } else {
-      price = await satoshisToFiat(bid.bid, 'USD')
-    }
-
-    setPriceInFiat(price)
-  })()
 
   return priceInFiat
 }
