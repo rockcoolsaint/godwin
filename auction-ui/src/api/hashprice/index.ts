5be4b6ac7ba7d87eb03977437198ff8b3d53f661
Jeezman <tobiadeyemi91@gmail.com>
2023-04-28 15:21:31 +0100

    hash fix

--- auction-ui/src/api/hashprice/index.ts
+++ auction-ui/src/api/hashprice/index.ts
@@ -13,26 +13,14 @@ export async function getBlockTipHeight() {
 }
 
 export interface HashRateResponse {
-  avg_fees_per_block: number
-  current_hashrate: number
-  current_hashrate_estimated: number
-  fees_percent: number
-  hash_price: number
-  hash_rate_30: number
-  hash_value: number
-  monthly_avg_hashrate_change_1_year: MonthlyAvgHashrateChange1Year
-  rev_usd: number
-}
-
-export interface MonthlyAvgHashrateChange1Year {
-  absolute: number
-  relative: number
+  hashrates: []
+  difficulty: []
+  currentHashrate: number
+  currentDifficulty: number
 }
 
 export async function getHashRate() {
-  const data = await fetch(`https://insights.braiins.com/api/v1.0/hashrate-stats`)
+  const data: HashRateResponse = await get(`https://mempool.space/api/v1/mining/hashrate/1m`)
 
-  const _data: HashRateResponse = await data.json()
-
-  return _data
+  return data
 }
