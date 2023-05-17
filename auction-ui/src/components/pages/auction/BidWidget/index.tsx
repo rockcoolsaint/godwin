b024d4218aaccee7a0d4b70513f4f1ad59d9f357
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-05-17 17:22:21 +0200

    fix: use starting_bid instead of current_bid, make sure default value uses min increment

--- auction-ui/src/components/pages/auction/BidWidget/index.tsx
+++ auction-ui/src/components/pages/auction/BidWidget/index.tsx
@@ -103,7 +103,7 @@ const BidWidget = ({ auction, current_bid, bids }: Props) => {
   }, [])
 
   useEffect(() => {
-    setValue('bid', bids[0]?.bid || auction?.current_bid)
+    setValue('bid', bids[0]?.bid + 1000 || auction?.starting_bid)
   }, [bids])
 
   return (
@@ -160,7 +160,7 @@ const BidWidget = ({ auction, current_bid, bids }: Props) => {
                       <form className="gap-4" onSubmit={handleSubmit(handlePlaceBid)}>
                         <Input
                           id="bid"
-                          defaultValue={auction.current_bid}
+                          defaultValue={bids[0]?.bid + 1000 || auction.starting_bid}
                           errorMessage={errors.bid?.message}
                           placeholder="Bid amount"
                           {...register('bid')}
