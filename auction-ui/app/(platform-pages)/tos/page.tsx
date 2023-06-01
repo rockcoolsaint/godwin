f6be99a8103a7dd9afd615baaea1aefc111aeb6b
Jeezman <tobiadeyemi91@gmail.com>
2023-06-01 15:59:59 +0100

    priv policy

--- auction-ui/app/(platform-pages)/tos/page.tsx
+++ auction-ui/app/(platform-pages)/tos/page.tsx
@@ -3,7 +3,7 @@ import Container from 'src/core/components/Container'
 export default function TOS() {
   return (
     <Container className="my-24">
-      <div className="mb-24">
+      <div className="mb-14">
         <div className="relative isolate overflow-hidden bg-gradient px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
           <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">Terms of Service</h2>
         </div>
