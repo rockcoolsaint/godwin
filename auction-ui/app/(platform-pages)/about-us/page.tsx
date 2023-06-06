3345d9ef0adb804083226bad4de28c6ca2e39147
Jeezman <tobiadeyemi91@gmail.com>
2023-06-06 16:05:26 +0100

    update

--- auction-ui/app/(platform-pages)/about-us/page.tsx
+++ auction-ui/app/(platform-pages)/about-us/page.tsx
@@ -4,6 +4,7 @@ import trustlessMining from 'src/assets/webp/trustless_mining.webp'
 import sla from 'src/assets/webp/sla.webp'
 import damian from 'src/assets/jpg/damian.jpg'
 import karo from 'src/assets/png/karo.png'
+import kevin from 'src/assets/png/kevin.png'
 import tobi from 'src/assets/png/tobi.png'
 import evan from 'src/assets/webp/evan.webp'
 import jonas from 'src/assets/webp/jonas.webp'
@@ -124,7 +125,7 @@ function AboutUs() {
               </p>
             </li>
             <li className="rounded-2xl bg-gray-800 px-8 py-10">
-              <Image className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src={damian} width={100} height={100} alt="Adam Jonas" />
+              <Image className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src={kevin} width={100} height={100} alt="Adam Jonas" />
               <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">Kevin Karsopawiro</h3>
               <p className="text-sm leading-6 text-gray-400">
                 Kevin is an experienced software engineer with over a decade of industry experience. Prior to joining Rigly, he spearheaded
