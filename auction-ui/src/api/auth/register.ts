9522b5720934b9cd9de628f22975dd888b16c7d8
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-06-13 21:23:58 +0200

    feat: demo registration

--- auction-ui/src/api/auth/register.ts
+++ auction-ui/src/api/auth/register.ts
@@ -6,6 +6,7 @@ interface RegisterPayload {
   mining_pool_address: string
   referral_code?: string
   create_pool_account: boolean
+  is_demo?: boolean
 }
 
 export async function register(payload: RegisterPayload, returnUrl?: string): Promise<[boolean, string | undefined]> {
