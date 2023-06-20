88fb7b83538f10839ba93baf549b3e889ad87c00
Jeezman <tobiadeyemi91@gmail.com>
2023-06-20 18:45:41 +0100

    enable forward date auction

--- auction-ui/src/api/auction/getAllAuctions.ts
+++ auction-ui/src/api/auction/getAllAuctions.ts
@@ -12,7 +12,7 @@ interface Props {
 export async function getAllAuctions({ limit, offset = 0, sorting = 'desc' }: Props): Promise<AllAuctionsResponse> {
   const auctions: AllAuctionsResponse = await makeClientRequest({
     method: 'GET',
-    path: `/api/auctions?limit=${limit}&offset=${offset}&sorting=${sorting}&demo=true`,
+    path: `/api/auctions?limit=${limit}&offset=${offset}&sorting=${sorting}`,
   })
 
   return auctions
--- auction-ui/src/api/auction/getFeaturedAuctions.ts
+++ auction-ui/src/api/auction/getFeaturedAuctions.ts
@@ -4,7 +4,7 @@ import { makeServerRequest } from 'src/api/serverRequest'
 export async function getFeaturedAuctions(): Promise<Auction[]> {
   const featuredAuction: Auction[] = await makeServerRequest({
     method: 'GET',
-    path: `/api/auctions/featured?limit=6&demo=true`,
+    path: `/api/auctions/featured?limit=6`,
     nextFetchRequestConfig: {
       revalidate: 0,
     },
