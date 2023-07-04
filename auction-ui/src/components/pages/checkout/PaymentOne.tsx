cbc4977464050b61a0239f120f27664d660fe009
Jeezman <tobiadeyemi91@gmail.com>
2023-07-04 22:49:41 +0100

    fix

--- auction-ui/src/components/pages/checkout/PaymentOne.tsx
+++ auction-ui/src/components/pages/checkout/PaymentOne.tsx
@@ -159,8 +159,8 @@ function PaymentOne({ order }: Props) {
     currentOrder.payments.length > 0
       ? currentOrder.payments
           .filter(payment => payment.provider === PaymentProvider.OpenNode)
-          .reduce((highest, current) => (current.id > highest.id ? current : highest), { id: 0 })
-      : undefined
+          .reduce((highest, current) => (current.id > highest.id ? current : highest))
+      : currentOrder.payments[0]
 
   const showRemaining =
     currentOrder &&
