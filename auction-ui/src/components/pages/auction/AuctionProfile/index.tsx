2e840420b8011b5600ea499919b14d2c0937bc97
Jeezman <tobiadeyemi91@gmail.com>
2023-07-06 23:10:31 +0100

    Add estimated start date for epoch on Profile tab

--- auction-ui/src/components/pages/auction/AuctionProfile/index.tsx
+++ auction-ui/src/components/pages/auction/AuctionProfile/index.tsx
@@ -2,6 +2,8 @@
 import { useTranslation } from 'src/hooks'
 import { Auction } from 'src/api/auction/types'
 import { underscoreToSpaceAndCapitalize } from 'utils'
+import { formatDate } from 'src/utils/date'
+
 interface Props {
   data: Auction
 }
@@ -12,9 +14,9 @@ const AuctionProfile = ({ data }: Props) => {
   return (
     <section className="rounded-3 flow-root h-full rounded-xl border bg-gray-50 px-0 py-3 sm:px-4">
       <div className="border-1 flex justify-between rounded-xl bg-white p-2">
-        <div className="flex-1 ">
+        <div className="flex-1">
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
-            <aside className="w-3/6 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-2/6">
+            <aside className="sm:w-1/6 md:w-1/3 lg:w-2/6 w-3/6 border-r-2 border-white">
               <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.type')}</p>
             </aside>
             <aside className="w-3/6">
@@ -22,13 +24,21 @@ const AuctionProfile = ({ data }: Props) => {
             </aside>
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
-            <aside className="w-3/6 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-2/6">
+            <aside className="sm:w-1/6 md:w-1/3 lg:w-2/6 w-3/6 border-r-2 border-white">
               <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.epoch')}</p>
             </aside>
             <aside>{data.epoch ? <p className="p-3 pl-4">{data.epoch.epoch_number}</p> : <p className="p-3 pl-4">-</p>}</aside>
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
-            <aside className="w-3/6 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-2/6">
+            <aside className="sm:w-1/6 md:w-1/3 lg:w-2/6 w-3/6 border-r-2 border-white">
+              <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.epoch')} start date</p>
+            </aside>
+            <aside>
+              <p className="p-3 pl-4">{formatDate(data.auction_meta.hashrate_start, 'MMMM d, yyyy')}</p>
+            </aside>
+          </div>
+          <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
+            <aside className="sm:w-1/6 md:w-1/3 lg:w-2/6 w-3/6 border-r-2 border-white">
               <p className="p-3 py-4 font-semibold capitalize text-dark-100">{t('home.duration')}</p>
             </aside>
             <aside>
@@ -38,7 +48,7 @@ const AuctionProfile = ({ data }: Props) => {
             </aside>
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
-            <aside className="w-3/6 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-2/6">
+            <aside className="sm:w-1/6 md:w-1/3 lg:w-2/6 w-3/6 border-r-2 border-white">
               <p className="p-3 py-4 font-semibold capitalize text-dark-100 sm:p-3">{t('home.hashrate')}</p>
             </aside>
             <aside>
@@ -46,7 +56,7 @@ const AuctionProfile = ({ data }: Props) => {
             </aside>
           </div>
           <div className="flex items-center border-b-2 border-white odd:bg-gray-300 even:bg-gray-50">
-            <aside className="w-3/6 border-r-2 border-white sm:w-1/6 md:w-1/3 lg:w-2/6">
+            <aside className="sm:w-1/6 md:w-1/3 lg:w-2/6 w-3/6 border-r-2 border-white">
               <p className="p-3 py-4 font-semibold text-dark-100">{t('home.power_source')}</p>
             </aside>
             <aside>
