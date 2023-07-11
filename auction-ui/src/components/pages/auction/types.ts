f8dd22793258ecdf264c1f86fac67a2cd0248261
Jeezman <tobiadeyemi91@gmail.com>
2023-07-11 14:36:00 +0100

    TS fixes

--- auction-ui/src/components/pages/auction/types.ts
+++
@@ -1,6 +0,0 @@
-import * as Plotly from 'plotly.js'
-
-export interface PlotDataType {
-  data: Plotly.Data[]
-  layout: Plotly.Layout
-}
--- auction-ui/src/components/pages/signup/index.tsx
+++ auction-ui/src/components/pages/signup/index.tsx
@@ -24,8 +24,8 @@ enum Step {
 
 interface FormInputs {
   email: string
-  referral_code: string
-  mining_pool_username: string
+  referral_code: string | undefined
+  mining_pool_username: string | undefined
   mining_pool_address: string
 }
 
@@ -385,31 +385,3 @@ const notificationMethods = [
   { id: 'yes', title: 'Yes' },
   { id: 'no', title: 'No' },
 ]
-
-export function HasPool() {
-  return (
-    <div>
-      <label className="text-base font-semibold text-gray-900">Notifications</label>
-      <p className="text-sm text-gray-500">How do you prefer to receive notifications?</p>
-      <fieldset className="mt-4">
-        <legend className="sr-only">Notification method</legend>
-        <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-          {notificationMethods.map(notificationMethod => (
-            <div key={notificationMethod.id} className="flex items-center">
-              <input
-                id={notificationMethod.id}
-                name="notification-method"
-                type="radio"
-                defaultChecked={notificationMethod.id === 'email'}
-                className="h-4 w-4 border-gray-300 text-primary focus:ring-primary"
-              />
-              <label htmlFor={notificationMethod.id} className="ml-3 block text-sm font-medium leading-6 text-gray-900">
-                {notificationMethod.title}
-              </label>
-            </div>
-          ))}
-        </div>
-      </fieldset>
-    </div>
-  )
-}
