137629465bc4614632ca05bb168f67599a8d41d8
Jeezman <tobiadeyemi91@gmail.com>
2023-07-11 16:59:19 +0100

    clean up

--- auction-ui/next.config.js
+++ auction-ui/next.config.js
@@ -106,37 +106,3 @@ module.exports = withSentryConfig(
     disableLogger: true,
   },
 )
-
-// module.exports = {
-//   webpack: (config, options) => {
-//     config.module.rules.push({
-//       test: /\.mdx/,
-//       use: [
-//         options.defaultLoaders.babel,
-//         {
-//           loader: '@mdx-js/loader',
-//           options: pluginOptions.options,
-//         },
-//       ],
-//     })
-
-//     return config
-//   },
-// }
-
-// module.exports = {
-//   webpack: config => {
-//     config.module.rules.push({
-//       test: /\.(mp3)$/,
-//       use: {
-//         loader: 'file-loader',
-//         options: {
-//           publicPath: '/_next/static/sounds/',
-//           outputPath: 'static/sounds/',
-//           name: '[name].[ext]',
-//           esModule: false,
-//         },
-//       },
-//     })
-//   },
-// }
