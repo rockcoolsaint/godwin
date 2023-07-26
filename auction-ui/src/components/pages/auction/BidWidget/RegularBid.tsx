f257d94ec14d798cccdb3c52229e1f3023e18687
Jeezman <tobiadeyemi91@gmail.com>
2023-07-26 12:45:07 +0100

    format bid input

--- auction-ui/src/components/pages/auction/BidWidget/RegularBid.tsx
+++ auction-ui/src/components/pages/auction/BidWidget/RegularBid.tsx
@@ -1,28 +1,16 @@
 import { useCallback, useEffect, useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
+import { SubmitHandler, useForm, Controller } from 'react-hook-form'
 import toast from 'react-hot-toast'
-
 import ws from 'src/lib/ws'
 import { Input } from 'src/core'
 import { useWebsocketContext } from 'src/providers/WebsocketProvider'
 import { Auction, BidsEntityOrCurrentBid } from 'src/api/auction/types'
+import { NumericFormat } from 'react-number-format'
+import clsx from 'clsx'
+import { transformCurrencyToNumber } from 'src/utils/currency'
 
 interface FormInputs {
   bid: number
-}
-
-const validationSchema = (value = 5000) => {
-  if (value <= 5000) {
-    value = 5000
-  } else {
-    value += 1000
-  }
-
-  return yup.object().shape({
-    bid: yup.number().integer().positive().min(value).required().typeError('bid must be a number'),
-  })
 }
 
 export default function RegularBid({
@@ -39,12 +27,11 @@ export default function RegularBid({
 
   const {
     reset,
-    register,
     handleSubmit,
     formState: { errors },
     setValue,
+    control,
   } = useForm<FormInputs>({
-    resolver: yupResolver(validationSchema(bids[0]?.bid || auction?.starting_bid)),
     defaultValues: {
       bid: bids[0]?.bid || auction?.starting_bid,
     },
@@ -57,7 +44,7 @@ export default function RegularBid({
 
         const res: any = await ws.request('place_bid', {
           auction_id: auction.id,
-          amount: value.bid,
+          amount: transformCurrencyToNumber(value.bid),
         })
 
         if (res.error) {
@@ -84,13 +71,45 @@ export default function RegularBid({
       <p className="text-base font-semibold text-dark-100">Enter your bid</p>
       <div className="mt-2 flex flex-col">
         <form className="gap-4" onSubmit={handleSubmit(handlePlaceBid)}>
-          <Input
-            id="bid"
-            defaultValue={bids[0]?.bid + 1000 || auction.starting_bid}
-            errorMessage={errors.bid?.message}
-            placeholder="Bid amount"
-            {...register('bid')}
+          <Controller
+            name="bid"
+            control={control}
+            render={({ field: { ref, onChange, ...rest } }) => {
+              return (
+                <NumericFormat
+                  decimalScale={0}
+                  onChange={e => {
+                    onChange(() => setValue('bid', transformCurrencyToNumber(e.target.value)))
+                  }}
+                  style={{ border: 'none', outline: 'none', boxShadow: 'none' }}
+                  className={clsx(
+                    errors.bid
+                      ? 'rounded-lg border border-error outline-none focus:border-none focus:border-error focus:ring-0'
+                      : 'rounded-lg border border-gray-500',
+                  )}
+                  getInputRef={ref}
+                  suffix=" Sats"
+                  thousandSeparator={true}
+                  allowNegative={false}
+                  customInput={Input}
+                  {...rest}
+                />
+              )
+            }}
+            rules={{
+              required: { value: true, message: 'Bid amount is required' },
+              min: {
+                value: bids[0]?.bid + 1000 || auction.starting_bid,
+                message: `Minimum bid is ${
+                  Number(bids[0]?.bid + 1000).toLocaleString() || Number(auction.starting_bid).toLocaleString()
+                } sats`,
+              },
+            }}
           />
+          {!!errors && (
+            <span className="line-clamp-2 translate-y-2 text-sm text-error transition-opacity">{errors.bid?.message as string}</span>
+          )}
+
           <button
             className="mt-8 flex h-12 w-full items-center justify-center rounded-lg bg-gradient px-5 text-white outline-none hover:bg-gradient-hover disabled:cursor-not-allowed disabled:bg-gradient-disabled"
             type="submit"
