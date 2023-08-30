73f0bf18f9558c3b54cf868611ea913638a39121
Jeezman <tobiadeyemi91@gmail.com>
2023-08-30 19:50:44 +0100

    remove suffix

--- auction-ui/app/(baltic-honeybadger)/riga/success/page.tsx
+++ auction-ui/app/(baltic-honeybadger)/riga/success/page.tsx
@@ -117,7 +117,7 @@ export default function BalticSuccessPage() {
         <div className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
           <dt className="text-sm font-medium leading-6 text-gray-500">Elapsed time</dt>
           <dd className="w-full flex-none break-all text-sm font-medium tracking-tight text-gray-900">
-            {formatDistance(parseISO(status!.assigned_at), new Date(), { addSuffix: true }) || '-'}
+            {formatDistance(parseISO(status!.assigned_at), new Date()) || '-'}
           </dd>
         </div>
         <div className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
