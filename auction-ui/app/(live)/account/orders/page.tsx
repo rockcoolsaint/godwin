33536cc00a05d56f78f5253c12c62e10782de4c8
Jeezman <tobiadeyemi91@gmail.com>
2023-08-31 18:03:53 +0100

    - add collapsible to table - improve payments view

--- auction-ui/app/(live)/account/orders/page.tsx
+++ auction-ui/app/(live)/account/orders/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 import AccountView from 'src/components/pages/account/AccountView'
 import Link from 'src/components/shared/Link'
 import { getOrders } from 'src/api/account/getOrders'
-import { Order, OrderStatus, OrderType, Payment } from 'src/types'
+import { Order, OrderStatus, OrderType } from 'src/types'
 import { Loader, Table } from 'src/core'
 import { useAccountContext } from 'src/providers/AccountProvider'
 import protect from 'src/hoc/protect'
@@ -14,14 +14,9 @@ import hasPassedOrderStatus from 'src/utils/hasPassedOrderStatus'
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import clsx from 'clsx'
-
-import Modal from 'src/core/components/Modal'
-import { formatMoney } from 'src/utils/currency'
-import SatsSvg from 'src/assets/svg/sats.svg'
 import { useRouter } from 'next/navigation'
-import mempoolTxUrl from 'src/utils/mempoolUrl'
 
-function formatOrderStatus(status: string) {
+export function formatOrderStatus(status: string) {
   switch (status) {
     case OrderStatus.Unpaid:
       return <span className="font-semibold text-red-600">Unpaid</span>
@@ -41,21 +36,6 @@ function Orders() {
 
   const [loading, setLoading] = useState<boolean>(true)
   const [orders, setOrders] = useState<Order[]>([])
-  const [payments, setPayments] = useState<Payment[]>([])
-  const [showPaymentModal, setShowPaymentModal] = useState<boolean>(false)
-
-  const handleCloseModal = () => {
-    setShowPaymentModal(false)
-  }
-
-  const handleShowPaymentModal = (id: number) => {
-    const filteredPayments = orders.filter(order => {
-      return order.id === id
-    })
-
-    setPayments(filteredPayments[0].payments)
-    setShowPaymentModal(true)
-  }
 
   useEffect(() => {
     const prepareOrders = async () => {
@@ -120,7 +100,7 @@ function Orders() {
                 case 'actions': {
                   return (
                     <div className="flex h-full items-center justify-end">
-                      <OrderAction order={order} hasManageAccess={hasManageAccess} handleShowPaymentModal={handleShowPaymentModal} />
+                      <OrderAction order={order} hasManageAccess={hasManageAccess} />
                     </div>
                   )
                 }
@@ -130,22 +110,13 @@ function Orders() {
           />
         )}
       </AccountView>
-      <OrderPayments payments={payments} open={showPaymentModal} onClose={handleCloseModal} />
     </>
   )
 }
 
 export default protect(Orders)
 
-export function OrderAction({
-  order,
-  hasManageAccess,
-  handleShowPaymentModal,
-}: {
-  order: Order
-  hasManageAccess: boolean
-  handleShowPaymentModal: (id: number) => void
-}) {
+function OrderAction({ order, hasManageAccess }: { order: Order; hasManageAccess: boolean }) {
   const router = useRouter()
 
   return (
@@ -193,16 +164,6 @@ export function OrderAction({
             <Menu.Item>
               {({ active }) => (
                 <span
-                  onClick={() => handleShowPaymentModal(order.id)}
-                  className={clsx(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}
-                >
-                  View Payments
-                </span>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <span
                   onClick={() => router.push(`/account/orders/${order.id}`)}
                   className={clsx(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}
                 >
@@ -214,61 +175,5 @@ export function OrderAction({
         </Menu.Items>
       </Transition>
     </Menu>
-  )
-}
-
-export function OrderPayments({ payments, open, onClose }: { payments: Payment[]; open: boolean; onClose: () => void }) {
-  return (
-    <Modal open={open} onClose={onClose} className="min-w-[420px]">
-      <Modal.Header>
-        <Modal.Title>Payments</Modal.Title>
-        <Modal.Close />
-      </Modal.Header>
-
-      <Modal.Content className="w-full">
-        <Table
-          data={payments}
-          cols={[
-            { title: 'ID', name: 'id' },
-            { title: 'Amount', name: 'amount' },
-            { title: 'Provider', name: 'provider' },
-            { title: 'Status', name: 'status' },
-            { title: 'Tx ID', name: 'tx_id' },
-          ]}
-          row={(payment, col) => {
-            switch (col) {
-              case 'id': {
-                return <div className="flex h-12 items-center">{payment.id}</div>
-              }
-              case 'amount': {
-                return (
-                  <div className="flex h-12 items-center">
-                    {formatMoney(payment.amount)} <SatsSvg className="ml-2" />
-                  </div>
-                )
-              }
-              case 'provider': {
-                return <div className="flex h-12 items-center">{payment.provider}</div>
-              }
-              case 'status': {
-                return <div className="flex h-12 items-center">{formatOrderStatus(payment.status)}</div>
-              }
-              case 'tx_id': {
-                return (
-                  <Link
-                    target="_blank"
-                    href={mempoolTxUrl(payment.tx_id)}
-                    className="flex h-12 items-center text-blue-500 underline hover:no-underline"
-                  >
-                    {payment.tx_id}
-                  </Link>
-                )
-              }
-            }
-          }}
-          empty={() => <span className="text-sm text-gray-500">No payments recorded</span>}
-        />
-      </Modal.Content>
-    </Modal>
   )
 }
