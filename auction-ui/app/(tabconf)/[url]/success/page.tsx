951f6d0be6c6541edd5f8af0951950d16f81b6e4
Jeezman <tobiadeyemi91@gmail.com>
2023-09-14 14:14:25 +0100

    fix

--- auction-ui/app/(tabconf)/[url]/success/page.tsx
+++ auction-ui/app/(tabconf)/[url]/success/page.tsx
@@ -111,7 +111,9 @@ export default function BalticSuccessPage({ params }: { params: any }) {
           </div>
           <div className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
             <dt className="text-sm font-medium leading-6 text-gray-500">Account</dt>
-            <dd className="w-full flex-none break-all text-sm font-medium tracking-tight text-gray-900">riglycorp</dd>
+            <dd className="w-full flex-none break-all text-sm font-medium tracking-tight text-gray-900">
+              {status?.pool_user?.username || '-'}
+            </dd>
           </div>
           <div className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8">
             <dt className="text-sm font-medium leading-6 text-gray-500">Elapsed time</dt>
