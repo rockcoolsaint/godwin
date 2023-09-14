6b82abd1d58cd4514a45229b25b9e261b40857aa
Jeezman <tobiadeyemi91@gmail.com>
2023-09-14 13:49:50 +0100

    update create order

--- auction-ui/src/api/orders/createOrder.ts
+++ auction-ui/src/api/orders/createOrder.ts
@@ -3,14 +3,16 @@ import { Order } from 'src/types'
 
 interface CreateOrderPayload {
   account_id: number | undefined
+  amount_sats?: number
+  duration_days?: number
 }
 
-export async function createOrder(payload: CreateOrderPayload): Promise<Order> {
+export async function createOrder({ account_id, amount_sats = 500, duration_days = 0.125 }: CreateOrderPayload): Promise<Order> {
   try {
     const res = await makeClientRequest({
       method: 'POST',
       path: `/api/orders/create`,
-      body: { ...payload, order_type: 'direct', amount_sats: 500, duration_days: 0.125 },
+      body: { account_id: account_id, order_type: 'direct', amount_sats: amount_sats, duration_days: duration_days },
     })
 
     return res
