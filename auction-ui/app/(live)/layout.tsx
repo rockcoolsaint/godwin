83293a59d5b6f6472f485c881d996df6857630da
Jeezman <tobiadeyemi91@gmail.com>
2023-09-19 17:18:02 +0100

    add beta banner

--- auction-ui/app/(live)/layout.tsx
+++ auction-ui/app/(live)/layout.tsx
@@ -1,20 +1,12 @@
 import DemoAlert from 'src/components/demo/Alert'
 import Footer from 'src/components/shared/Footer'
 import Header from 'src/components/shared/Header'
-import Link from 'src/components/shared/Link'
 
 export default function LiveLayout({ children }: { children: React.ReactNode }) {
   return (
     <section>
       <Header isDemo={false} />
-      {/* <DemoAlert msg="Product in beta please report bugs using the intercom below" /> */}
-      <DemoAlert
-        msg={
-          <div>
-            Tabconf attendees: <Link href="https://rigly.io/tabconf">sign up here</Link> for your hashrate
-          </div>
-        }
-      />
+      <DemoAlert msg="Product in beta please report bugs using the intercom below" />
 
       {children}
       <Footer />
