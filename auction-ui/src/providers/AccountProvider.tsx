fcae45c36ca37da1319c751c770461d493fcf372
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2023-10-06 19:06:12 +0200

    fix: auth

--- auction-ui/src/providers/AccountProvider.tsx
+++ auction-ui/src/providers/AccountProvider.tsx
@@ -94,9 +94,10 @@ export default function AccountProvider({ children }: { children: React.ReactNod
       const authorize = async () => {
         // TODO: Implement refresh token logic instead of just accepting the account if it exists in state.
 
-        if (account) {
-          return
-        }
+        // TODO: The code below interferes with some of our token logic according to @Jeezman so for now we disable it.
+        // if (account) {
+        //   return
+        // }
 
         try {
           setIsLoading(true)
@@ -117,7 +118,7 @@ export default function AccountProvider({ children }: { children: React.ReactNod
       }
       authorize()
     }
-  }, [pathName, router, account])
+  }, [pathName, router])
 
   return <AccountContext.Provider value={{ account, token, isLoading, login, logout, refresh }}>{children}</AccountContext.Provider>
 }
