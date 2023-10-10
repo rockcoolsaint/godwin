29de5b38ea98f830c3db410d5557fe6cbe80a3ac
Jeezman <tobiadeyemi91@gmail.com>
2023-10-10 18:20:44 +0100

    review update

--- auction-ui/src/components/pages/home/Calculator/index.tsx
+++ auction-ui/src/components/pages/home/Calculator/index.tsx
@@ -4,7 +4,6 @@ import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline'
 import { useEffect, useState } from 'react'
 import { getHashRate } from 'src/api/hashprice'
 import { Tooltip, TooltipContent, TooltipTrigger } from 'src/components/shared/Tooltip'
-import { Loader } from 'src/core'
 
 export function HomepageCalculator() {
   const [difficulty, setDifficulty] = useState(0)
@@ -13,7 +12,8 @@ export function HomepageCalculator() {
   const [hashrateRaw, setHashrateRaw] = useState(0)
 
   const [hashprice, setHashprice] = useState(0)
-  const [loading, setLoading] = useState(false)
+  const [baseHashprice, setBaseHashprice] = useState(0)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -38,13 +38,55 @@ export function HomepageCalculator() {
     const blockReward = 6.25
     const txFee = feePercentage / 100
     const hashprice = (blockReward * 144 + blockReward * 144 * txFee) / (globalHashrate * 1_000_000_000_000)
+    const baseHashprice = (blockReward * 144) / hashrateRaw
     const difficulty = (globalHashrate * 1_000_000_000_000_000_000 * 600) / 2 ** 32
-    setHashprice(Math.floor(hashprice * 100000000000000))
+    setHashprice(Math.floor(hashprice * 100_000_000_000_000))
+    setBaseHashprice(Math.floor(baseHashprice * 100_000_000_000_000_000_000))
     setDifficulty(difficulty / 1_000_000_000_000)
   }, [globalHashrate, hashrateRaw, feePercentage])
 
   if (loading) {
-    return <Loader />
+    return (
+      <div className="min-h-96 mx-auto h-96 rounded-md border-2 border-dark-100 p-8 shadow sm:w-8/12">
+        <div className="flex animate-pulse flex-col ">
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 rounded bg-slate-300"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
+                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
+              </div>
+              <div className="h-2 rounded bg-slate-300"></div>
+            </div>
+          </div>
+          <div className="mt-8 flex-1 space-y-6 py-1">
+            <div className="h-2 rounded bg-slate-300"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
+                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
+              </div>
+              <div className="h-2 rounded bg-slate-300"></div>
+            </div>
+          </div>
+          <div className="mt-8 flex-1 space-y-6 py-1">
+            <div className="h-2 rounded bg-slate-300"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="col-span-2 h-2 rounded bg-slate-300"></div>
+                <div className="col-span-1 h-2 rounded bg-slate-300"></div>
+              </div>
+              <div className="h-2 rounded bg-slate-300"></div>
+            </div>
+          </div>
+          <div className="mt-8 w-11/12 flex-1 items-center justify-center">
+            <div className="mx-auto w-6/12">
+              <div className="h-8 w-full rounded bg-slate-300"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return (
@@ -52,7 +94,7 @@ export function HomepageCalculator() {
       <div className="relative flex w-full flex-col items-center justify-between rounded-md border-2 border-dark-100 bg-white p-4 shadow-2xl sm:w-9/12 sm:pl-10 sm:pr-0">
         <div className="w-full">
           <aside className="my-4 mt-0 flex items-center justify-between">
-            <div className="flex w-6/12 items-center">
+            <div className="flex w-6/12 items-start">
               <span className="text-sm">Global Network hashrate</span>
               <Tooltip placement="top">
                 <TooltipTrigger>
@@ -68,7 +110,7 @@ export function HomepageCalculator() {
             <span className="block w-4/12 text-sm font-semibold">{globalHashrate} EH/s</span>
           </aside>
           <aside className="mb-4 flex items-center justify-between">
-            <div className="flex w-6/12 items-center">
+            <div className="flex w-6/12 items-start">
               <span className="text-sm">Difficulty estimate</span>
               <Tooltip placement="top">
                 <TooltipTrigger>
@@ -85,7 +127,7 @@ export function HomepageCalculator() {
             </span>
           </aside>
           <aside className="mb-4 flex items-center justify-between">
-            <div className="flex w-6/12 items-center">
+            <div className="flex w-6/12 items-start">
               <span className="text-sm">Hashprice estimate</span>
               <Tooltip placement="top">
                 <TooltipTrigger>
@@ -100,10 +142,19 @@ export function HomepageCalculator() {
             </div>
           </aside>
           <aside className="mb-4 ml-4 flex items-center justify-between">
-            <div className="flex w-4/12 items-center">
+            <div className="flex w-4/12 items-start">
               <span className="text-xs">Base hashprice</span>
+              <Tooltip placement="top">
+                <TooltipTrigger>
+                  <QuestionMarkCircleIcon className="ml-1 h-4 w-4" />
+                </TooltipTrigger>
+
+                <TooltipContent className="w-fit rounded bg-gray-600 px-2 py-1 text-xs font-medium text-white">
+                  Block subsidy only
+                </TooltipContent>
+              </Tooltip>
             </div>
-            <span className="mr-2 w-4/12 text-xs font-semibold">{Math.floor(hashprice)} sats/TH/s/day</span>
+            <span className="mr-2 w-4/12 text-xs font-semibold">{baseHashprice} sats/TH/s/day</span>
           </aside>
           <aside className="mb-4 ml-4 flex items-center justify-between">
             <div className="flex w-6/12 items-center">
@@ -112,10 +163,19 @@ export function HomepageCalculator() {
             <span className="mr-2 w-4/12 text-xs font-semibold">{feePercentage}%</span>
           </aside>
           <aside className="mb-4 ml-4 flex items-center justify-between">
-            <div className="flex w-4/12 items-center">
+            <div className="flex w-4/12 items-start">
               <span className="text-sm font-semibold">Total</span>
+              <Tooltip placement="top">
+                <TooltipTrigger>
+                  <QuestionMarkCircleIcon className="ml-1 h-4 w-4" />
+                </TooltipTrigger>
+
+                <TooltipContent className="w-fit rounded bg-gray-600 px-2 py-1 text-xs font-medium text-white">
+                  Total hashprice (with tx fees, based on {feePercentage}% fee percentage)
+                </TooltipContent>
+              </Tooltip>
             </div>
-            <p className="mr-2 w-4/12 text-sm font-semibold">{Math.floor(hashprice) + 10} TH/s/day</p>
+            <p className="mr-2 w-4/12 text-sm font-semibold">{Math.floor(hashprice)} sats/TH/s/day</p>
           </aside>
         </div>
         <p className="mb-4 mt-8 text-center text-sm">Adjust global network hashrate</p>
