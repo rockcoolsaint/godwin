869a59db66bfbc3cb3d6460dd916fa8e23ad8c39
Jeezman <tobiadeyemi91@gmail.com>
2023-10-24 04:16:12 +0100

    Update onchain details

--- auction-ui/src/components/pages/block-party/OnchainDetails.tsx
+++ auction-ui/src/components/pages/block-party/OnchainDetails.tsx
@@ -1,55 +1,72 @@
-const people2 = [
-  {
-    name: 'Pending Balance',
-    email: 'michael.foster@example.com',
-    role: '0',
-    imageUrl: '',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-  {
-    name: 'Escrow Balance',
-    email: 'dries.vincent@example.com',
-    role: '0',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: null,
-  },
-  {
-    name: 'Escrow Address',
-    email: 'lindsay.walton@example.com',
-    role: '1GVY5eZvtc5bA6EFEGnpqJeHUC5YaV5dsb',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-  {
-    name: 'Host',
-    email: 'courtney.henry@example.com',
-    role: 'Kevin',
-    imageUrl:
-      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-]
+import Link from 'src/components/shared/Link'
+import { BlockPartyOnchain } from 'src/types'
+import { formatMoney } from 'src/utils/currency'
 
-function BlockPartyOnchainDetails() {
+function BlockPartyOnchainDetails({ onchain }: { onchain: BlockPartyOnchain }) {
   return (
     <ul role="list" className="divide-y divide-gray-100">
-      {people2.map(person => (
-        <li key={person.email} className="flex justify-between gap-x-6 py-5">
-          <div className="flex min-w-0 gap-x-4">
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-            </div>
+      <li className="flex justify-between gap-x-6 py-5">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">Pending balance</p>
           </div>
-          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p className="text-sm leading-6 text-gray-900">{formatMoney(onchain.pending_balance)} sats</p>
+        </div>
+      </li>
+      <li className="flex justify-between gap-x-6 py-5">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">Escrow balance</p>
           </div>
-        </li>
-      ))}
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p className="text-sm leading-6 text-gray-900">{formatMoney(onchain.escrow_balance)}</p>
+        </div>
+      </li>
+      <li className="flex justify-between gap-x-6 py-5">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">Escrow address</p>
+          </div>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <Link
+            href={`https://mempool.space/address/${onchain.escrow_address}`}
+            target="_blank"
+            className="text-sm leading-6 underline hover:no-underline"
+          >
+            {onchain.escrow_address}
+          </Link>
+        </div>
+      </li>
+      <li className="flex justify-between gap-x-6 py-5">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">CKPool Stats</p>
+          </div>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <Link
+            href={`https://solo.ckpool.org/users/${onchain.escrow_address}`}
+            target="_blank"
+            className="text-sm leading-6 underline hover:no-underline"
+          >
+            {onchain.escrow_address}
+          </Link>
+        </div>
+      </li>
+      <li className="flex justify-between gap-x-6 py-5">
+        <div className="flex min-w-0 gap-x-4">
+          <div className="min-w-0 flex-auto">
+            <p className="text-sm font-semibold leading-6 text-gray-900">Block Party Host</p>
+          </div>
+        </div>
+        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+          <p className="text-sm leading-6 text-gray-900">{onchain.host.username}</p>
+        </div>
+      </li>
     </ul>
   )
 }
