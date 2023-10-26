1f623c784132d370ae2bcd07ef51a2c688af8b9b
Jeezman <tobiadeyemi91@gmail.com>
2023-10-26 19:21:28 +0100

    update order key

--- src/api/auction/getAllAuctions.ts
+++ src/api/auction/getAllAuctions.ts
@@ -23,7 +23,7 @@ export async function getAllAuctions({
   const auctions: AllAuctionsResponse = await makeClientRequest({
     method: 'GET',
     path:
-      `/api/auctions?limit=${limit}&offset=${offset}&sorting=${sorting}` +
+      `/api/auctions?limit=${limit}&offset=${offset}&order=${sorting}` +
       (auction_type ? `&auction_type=${auction_type}` : ``) +
       (auction_status ? `&auction_status=${auction_status}` : ``) +
       (sort_by ? `&sort_by=${sort_by}` : ``) +
