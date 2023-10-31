e794ce6ce35c28ccb2c8fb25c97c541286448878
Jeezman <tobiadeyemi91@gmail.com>
2023-10-31 14:22:55 +0100

    update

--- src/components/pages/block-party/OnchainDetails.tsx
+++ src/components/pages/block-party/OnchainDetails.tsx
@@ -47,7 +47,7 @@ function BlockPartyOnchainDetails({ onchain }: { onchain: BlockPartyOnchain }) {
         <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
           <Tooltip placement="top-end">
             <TooltipTrigger>
-              <p className="text-sm leading-6 text-gray-900">{formatMoney(onchain.escrow_balance)}</p>{' '}
+              <p className="text-sm leading-6 text-gray-900">{formatMoney(onchain.escrow_balance)} sats</p>{' '}
             </TooltipTrigger>
             <TooltipContent className="rounded bg-gray-600 p-2 text-xs font-medium text-white">
               ${formatMoney(escrowBalanceFiat)}
