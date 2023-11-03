14dbeeee6300eb787bfefaa0abfd9e889a21c0ea
Jeezman <tobiadeyemi91@gmail.com>
2023-11-03 17:19:18 +0100

    add auction schedule to auction market page

--- app/(live)/auction-market/page.tsx
+++ app/(live)/auction-market/page.tsx
@@ -1,9 +1,22 @@
-const AuctionMarketPage = () => {
+import { Suspense } from 'react'
+import { getAllAuctions } from 'src/api/auction/getAllAuctions'
+import AuctionSchedule from 'src/components/pages/home/AuctionSchedule'
+import { TableSkeletonLoader } from 'src/components/shared/TableSkeletonLoader'
+import Container from 'src/core/components/Container'
+
+export default async function AuctionMarketPage() {
+  const auction = await getAllAuctions({
+    limit: 1_000,
+    auction_type: 'forward_date',
+    group_by: 'auction_status',
+    auction_status: 'active',
+  })
+
   return (
-    <div>
-      <h1>Auction market</h1>
-    </div>
+    <Container className="py-12 xl:w-full">
+      <Suspense fallback={<TableSkeletonLoader title="Auction Market" />}>
+        <AuctionSchedule auctionsData={auction.results} />
+      </Suspense>
+    </Container>
   )
 }
-
-export default AuctionMarketPage
--- src/components/pages/home/AuctionSchedule/index.tsx
+++ src/components/pages/home/AuctionSchedule/index.tsx
@@ -182,7 +182,7 @@ export default function AuctionSchedule({ auctionsData }: { auctionsData: Auctio
   }
 
   return (
-    <section id="auction-market" className="elegant-gradient mt-28 pb-24 pt-28 sm:mt-0 sm:pt-0">
+    <section id="auction-market" className="mt-28 pb-24 pt-28 sm:mt-0 sm:pt-0">
       <Container className="w-12/12 flex items-center justify-center !px-1 sm:w-8/12">
         <section className="flex flex-col items-center justify-center sm:pl-0">
           <h1 className="mb-4 text-primary">Auction Market</h1>
