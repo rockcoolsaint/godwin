a7151bde1be0ee6dacdda4211a7bf9eaa1eb01c9
Jeezman <tobiadeyemi91@gmail.com>
2023-11-07 16:56:23 +0100

    Add revalidate constant to collection pages

--- app/(live)/collections/page.tsx
+++ app/(live)/collections/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 60
+
 import { getAllAuctions } from 'src/api/auction/getAllAuctions'
 import { sorting, defaultSort, auctionTypeFiltering, defaultType } from 'src/utils/constants'
 import CollectionList from 'src/components/pages/collections/CollectionsList'
