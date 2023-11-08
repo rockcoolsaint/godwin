56c5c869ece75f88336d967d5d72f53a23a47763
Jeezman <tobiadeyemi91@gmail.com>
2023-11-08 12:09:51 +0100

    Add error boundary to AuctionContainer

--- src/components/pages/auction/AuctionContainer/index.tsx
+++ src/components/pages/auction/AuctionContainer/index.tsx
@@ -22,6 +22,7 @@ import { LocalStorageKeys } from 'src/constants/localStorage'
 import { TAB_PANEL, TourState } from './types'
 import BreadCrumb from 'src/components/shared/BreadCrumb'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
+import { ErrorBoundary } from 'react-error-boundary'
 
 function tabClass({ selected }: { selected: boolean }) {
   return clsx(
@@ -181,46 +182,48 @@ export default function AuctionContainer({ auction, order, bids, current_bid, us
       />
       <p className="mb-2 text-base text-dark-100">{renderAuctionMeta()}</p>
       <section className="auction-container flex flex-col rounded-xl bg-gray-50 sm:p-3 lg:flex-row">
-        <div data-test-id="digest-step-settings-interval" className="lg:w-[75%]">
-          <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-            <Tab.Panels className="scrollbar-hide h-[600px] overflow-scroll">
-              <Tab.Panel className={panelClass()}>
-                <div className="scrollbar-hide relative h-full rounded-md">
-                  <AuctionBids bids={bids} />
-                </div>
-              </Tab.Panel>
-              <Tab.Panel className={panelClass()}>
-                <div className="scrollbar-hide relative h-full rounded-md">
-                  <AuctionProfile data={auction} />
-                </div>
-              </Tab.Panel>
-              <Tab.Panel className={panelClass()}>
-                <div className="scrollbar-hide relative h-full rounded-md">
-                  <AuctionLiveFeed auction={auction} />
-                </div>
-              </Tab.Panel>
-              <Tab.Panel className={panelClass()}>
-                <div className="scrollbar-hide relative h-full rounded-md">
-                  <AuctionHashPrice />
-                </div>
-              </Tab.Panel>
-            </Tab.Panels>
-            <Tab.List className="mt-4 flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-              <Tab ref={bidRef} className={tabClass}>
-                Bids
-              </Tab>
-              <Tab ref={profileRef} className={tabClass}>
-                Profile
-              </Tab>
-              <Tab ref={liveFeedRef} className={tabClass}>
-                Live feed
-              </Tab>
-              <Tab ref={hashPriceRef} className={tabClass}>
-                Hash price
-              </Tab>
-            </Tab.List>
-          </Tab.Group>
-        </div>
+        <ErrorBoundary fallback={<div className="p-8">⚠️ Oops! something went wrong</div>}>
+          <div data-test-id="digest-step-settings-interval" className="lg:w-[75%]">
+            <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+              <Tab.Panels className="scrollbar-hide h-[600px] overflow-scroll">
+                <Tab.Panel className={panelClass()}>
+                  <div className="scrollbar-hide relative h-full rounded-md">
+                    <AuctionBids bids={bids} />
+                  </div>
+                </Tab.Panel>
+                <Tab.Panel className={panelClass()}>
+                  <div className="scrollbar-hide relative h-full rounded-md">
+                    <AuctionProfile data={auction} />
+                  </div>
+                </Tab.Panel>
+                <Tab.Panel className={panelClass()}>
+                  <div className="scrollbar-hide relative h-full rounded-md">
+                    <AuctionLiveFeed auction={auction} />
+                  </div>
+                </Tab.Panel>
+                <Tab.Panel className={panelClass()}>
+                  <div className="scrollbar-hide relative h-full rounded-md">
+                    <AuctionHashPrice />
+                  </div>
+                </Tab.Panel>
+              </Tab.Panels>
+              <Tab.List className="mt-4 flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                <Tab ref={bidRef} className={tabClass}>
+                  Bids
+                </Tab>
+                <Tab ref={profileRef} className={tabClass}>
+                  Profile
+                </Tab>
+                <Tab ref={liveFeedRef} className={tabClass}>
+                  Live feed
+                </Tab>
+                <Tab ref={hashPriceRef} className={tabClass}>
+                  Hash price
+                </Tab>
+              </Tab.List>
+            </Tab.Group>
+          </div>
+        </ErrorBoundary>
         <div className=" mt-4 flex min-w-fit flex-col lg:ml-4 lg:mt-0 lg:w-[25%]">
           <BidWidget auction={auction} bids={bids} current_bid={current_bid} user_proxy_bid={user_proxy_bid} winner={winner} />
 
