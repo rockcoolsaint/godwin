e00f623e8f968b41f9c213b18a9c80d19dcb8f67
Jeezman <tobiadeyemi91@gmail.com>
2023-12-28 14:34:57 +0100

    mobile join mining pool

--- src/components/pages/home/JoinPool/TestDrive/index.tsx
+++ src/components/pages/home/JoinPool/TestDrive/index.tsx
@@ -132,35 +132,35 @@ export default function TestDrive() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1 className="text-center font-chakra text-7xl text-white">Join a mining pool</h1>
-      <p className="my-10 w-11/12 text-center font-epilogue text-4xl text-white">
+    <>
+      <h1 className="mt-10 w-8/12 text-center font-chakra text-4xl text-white lg:w-full lg:text-7xl">Join a mining pool</h1>
+      <p className="my-5 w-11/12 text-center font-epilogue text-xs text-white lg:my-10 lg:text-4xl">
         If you&apos;re new to bitcoin mining, take our test drive which includes 3 hours of sample hashrate, and a mining pool account.
       </p>
 
-      <div className="flex w-full flex-col items-center rounded-xl bg-white p-10 py-16">
-        <h2 className="bg-gradient-to-r from-[#5C3FAF] to-[#316AEF] bg-clip-text font-chakra text-7xl font-bold text-transparent">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex w-full flex-col items-center rounded-xl bg-white p-4 sm:p-10 lg:py-16">
+        <h2 className="bg-gradient-to-r from-[#5C3FAF] to-[#316AEF] bg-clip-text font-chakra text-2xl font-bold text-transparent lg:text-7xl">
           Get started
         </h2>
-        <p className="my-8 w-5/12 text-center font-epilogue text-xl font-normal text-gray-500">
+        <p className="my-4 w-full text-center font-epilogue text-xs font-normal text-gray-500 lg:my-8 lg:w-5/12 lg:text-xl">
           You&apos;ll be setting up a demo pool account with Braiins.pool. You can change pools later.
         </p>
-        <div className="mb-10 flex w-7/12 items-center justify-between">
+        <div className="flex w-11/12 items-center justify-between lg:mb-10 lg:w-7/12">
           <p className="flex flex-col items-center justify-center text-center">
-            <h5 className="text-3xl font-bold">Hashrate</h5>
-            <p className="text-xl">88 TH/s</p>
+            <h5 className="text-sm font-bold lg:text-3xl">Hashrate</h5>
+            <p className="text-xs lg:text-xl">88 TH/s</p>
           </p>
           <p className="flex flex-col items-center justify-center text-center">
-            <h5 className="text-3xl font-bold">Duration</h5>
-            <p className="text-xl">3 hours</p>
+            <h5 className="text-sm font-bold lg:text-3xl">Duration</h5>
+            <p className="text-xs lg:text-xl">3 hours</p>
           </p>
           <p className="flex flex-col items-center justify-center text-center">
-            <h5 className="text-3xl font-bold">Price</h5>
-            <p className="text-xl">500 sats</p>
+            <h5 className="text-sm font-bold lg:text-3xl">Price</h5>
+            <p className="text-xs lg:text-xl">500 sats</p>
           </p>
         </div>
-        <div className="flex w-8/12 items-end justify-between">
-          <div className="flex w-7/12 flex-col">
+        <div className="flex w-11/12 flex-col items-center justify-center lg:w-8/12 lg:flex-row lg:items-end lg:justify-between">
+          <div className="flex w-11/12 flex-col lg:w-7/12">
             <Input
               className="text-2xl text-gray-600"
               id="email"
@@ -177,7 +177,7 @@ export default function TestDrive() {
           <button
             disabled={!isDirty || !isValid}
             type="submit"
-            className="mt-8 flex h-12 w-4/12 items-center justify-center rounded-lg bg-hero-gradient px-5 py-2 font-chakra text-xl font-bold text-white outline-none hover:opacity-80 disabled:cursor-not-allowed disabled:bg-gradient-disabled"
+            className="mt-4 flex h-12 items-center justify-center rounded-lg bg-hero-gradient px-5 py-2 font-chakra text-xl font-bold text-white outline-none hover:opacity-80 disabled:cursor-not-allowed disabled:bg-gradient-disabled lg:mt-8 lg:w-4/12"
           >
             Buy sample hashrate
           </button>
@@ -188,7 +188,7 @@ export default function TestDrive() {
             {renderStatus()}
           </div>
         )}
-      </div>
-    </form>
+      </form>
+    </>
   )
 }
