140401da194085e9d47f4ef0a691c2cf65172f2e
Jeezman <tobiadeyemi91@gmail.com>
2024-01-12 16:12:49 +0100

    update component

--- app/(tabconf)/[url]/td/success/page.tsx
+++ app/(tabconf)/[url]/td/success/page.tsx
@@ -74,7 +74,7 @@ export default function TestDriveSuccessPage({ params }: { params: any }) {
   if (!proxyStatus || !orderId) {
     return (
       <Container className="flex h-screen flex-col items-center justify-center bg-slate-50 pt-12">
-        <NotFoundComponent message="Ooops! No detail for this order" return_url="/" />
+        <NotFoundComponent message="No detail for this order" return_url="/" />
       </Container>
     )
   }
@@ -82,7 +82,7 @@ export default function TestDriveSuccessPage({ params }: { params: any }) {
   if (!account?.email) {
     return (
       <Container className="flex h-screen flex-col items-center justify-center bg-slate-50 pt-12">
-        <NotFoundComponent message="Ooops! Please log in to view this order" return_url="/login" />
+        <NotFoundComponent message="Please log in to view this order" return_url="/login" />
       </Container>
     )
   }
@@ -90,7 +90,7 @@ export default function TestDriveSuccessPage({ params }: { params: any }) {
   if (account?.email !== proxyStatus.email) {
     return (
       <Container className="flex h-screen flex-col items-center justify-center bg-slate-50 pt-12">
-        <NotFoundComponent message="Ooops! You're not permitted to view this order" return_url="/" />
+        <NotFoundComponent message="You're not permitted to view this order" return_url="/" />
       </Container>
     )
   }
--- src/components/shared/NotFoundComponent/index.tsx
+++ src/components/shared/NotFoundComponent/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function NotFoundComponent({ message, return_url }: PropsWithChildren<Props>) {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <p className="text-2xl font-semibold">{`404 | ${message}`}</p>
+      <p className="text-2xl font-semibold">{`${message}`}</p>
       <Link className="text-sm underline hover:no-underline" href={return_url || '/'}>
         Return to homepage
       </Link>
