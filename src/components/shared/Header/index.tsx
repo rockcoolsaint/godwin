bfb394dc37a286c9e70434e6d1a12e623d6b286f
daviroo <david.borthwick@gmail.com>
2024-04-27 14:33:01 +0100

    fix: Fixed header issues

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,12 +1,10 @@
 'use client'
-
 import Link from 'src/components/shared/Link'
 import LogoSvg from 'src/assets/svg/logo_dark.svg'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Authorized from './Authorized'
 import Unauthorized from './Unauthorized'
 import { useAccountContext } from 'src/providers/AccountProvider'
-import Container from 'src/core/components/Container'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import useReturnUrl from 'src/hooks/useReturnUrl'
@@ -22,7 +20,6 @@ export default function HeaderNav({ isDemo }: Props) {
   const { account, isLoading, logout } = useAccountContext()
   const returnUrl = useReturnUrl()
   const homeURL = isDemo ? '/demo/' : '/'
-
   const [active, setActive] = useState<boolean>(false)
 
   const toggleMobileMenu = (): void => {
@@ -46,36 +43,40 @@ export default function HeaderNav({ isDemo }: Props) {
   return (
     <>
       <header className="h-20 min-h-[80px]">
-        <Container className="flex h-full items-center justify-center border-b sm:justify-between">
-          <div className="absolute inset-y-0 left-5 flex h-20 items-center lg:hidden">
-            <button
-              onClick={toggleMobileMenu}
-              className="inline-flex items-center justify-center rounded-md text-dark-200 hover:text-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-expanded={active}
-              aria-label="Toggle navigation menu"
-            >
-              {active ? (
-                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-              ) : (
-                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-              )}
-            </button>
-          </div>
-          <div className="relative flex items-center">
+        <div className="mx-auto flex h-full w-full max-w-screen-2xl items-center justify-between px-5 md:px-8 lg:px-12">
+          <div className="flex items-center">
+            <div className="mr-4 lg:hidden">
+              <button
+                onClick={toggleMobileMenu}
+                className="inline-flex items-center justify-center rounded-md text-dark-200 hover:text-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-expanded={active}
+                aria-label="Toggle navigation menu"
+              >
+                {active ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </button>
+            </div>
             <Link href={homeURL}>
               <LogoSvg />
             </Link>
+          </div>
+          <div className="hidden lg:block">
             <DesktopNav />
           </div>
-          {!isLoading && <div className="ml-8 hidden md:block">{account ? <Authorized /> : <Unauthorized isDemo={isDemo} />}</div>}
-        </Container>
+          {!isLoading && <div className="ml-8 hidden lg:block">{account ? <Authorized /> : <Unauthorized isDemo={isDemo} />}</div>}
+        </div>
       </header>
-      <MobileNav
-        active={active}
-        handleLogoutClick={handleLogoutClick}
-        handleLoginClick={handleLoginClick}
-        handleRegisterClick={handleRegisterClick}
-      />
+      <div className="lg:hidden">
+        <MobileNav
+          active={active}
+          handleLogoutClick={handleLogoutClick}
+          handleLoginClick={handleLoginClick}
+          handleRegisterClick={handleRegisterClick}
+        />
+      </div>
     </>
   )
 }
