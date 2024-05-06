ac5029729d54b552a5a83cc294fc3769e0933e18
daviroo <david.borthwick@gmail.com>
2024-05-06 10:22:44 +0100

    fix: Fixes after feedback

--- app/(live)/direct-sale/page.tsx
+++ app/(live)/direct-sale/page.tsx
@@ -10,8 +10,8 @@ export default function DirectSalePage() {
 
   return (
     <Container className="xl:w-full">
-      <section className="flex w-full flex-col items-center lg:px-20 lg:py-10">
-        <h1 className="bg-gradient-to-r from-[#5C3FAF] to-[#316AEF] bg-clip-text text-center text-4xl font-bold text-transparent lg:text-5xl 2xl:text-7xl">
+      <section className="flex w-full flex-col items-center py-10 lg:px-20">
+        <h1 className="max-w-4xl bg-gradient-to-r from-[#5C3FAF] to-[#316AEF] bg-clip-text text-center text-4xl font-bold text-transparent md:text-5xl lg:text-6xl 2xl:text-7xl">
           Buy Hashrate and Start mining now
         </h1>
         <p className="my-5 w-11/12 text-center font-epilogue text-sm md:text-base lg:w-9/12 lg:text-xl 2xl:text-3xl">
