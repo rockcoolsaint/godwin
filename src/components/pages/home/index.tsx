109caf58fce638332fb7d8e6f0eda04b7444a36b
Evan Baer <evan@evanbaer.com>
2024-05-08 17:05:36 -0400

    Updates to match prod: re-enable auction and disable direct sale hashrate

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -79,7 +79,7 @@ export default function Home({ isDemo, code }: Props) {
             <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
               <circle cx={1} cy={1} r={1} />
             </svg>
-            Our marketplace is undergoing maintenance. Hashrate sales are temporarily disabled.
+            Auctions are back! Mining test drives are temporarily unavailable.
           </a>
         </p>
       </div>
