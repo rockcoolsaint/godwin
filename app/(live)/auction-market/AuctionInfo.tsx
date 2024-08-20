5a348a4eb103d931340bbd3f4e7d9c7427cac9ca
Eric Eberhart <el.eberhart40@gmail.com>
2024-08-20 12:12:30 -0400

    fix:align-border-color

--- app/(live)/auction-market/AuctionInfo.tsx
+++ app/(live)/auction-market/AuctionInfo.tsx
@@ -42,7 +42,7 @@ export default function AuctionInfo() {
         <div className="item-center flex flex-col gap-4 self-stretch">
           <div className="flex flex-col gap-2">
             <div className="font-chakra text-lg text-navy lg:text-4xl">Live Daily Auctions</div>
-            <div className="flex flex-col gap-2 rounded-md border-2 border-black p-1 text-left text-xs">
+            <div className="flex flex-col gap-2 rounded-md border border-gray-400 p-1 text-left text-xs">
               <div>- Bid Anonymously</div>
               <div>- Compete in live daily auctions from 9 to 4, M to F</div>
               <div>- Price determined by the market</div>
