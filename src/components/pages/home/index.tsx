67e0aea5398d582b1b515e36e695ea9826123d6c
Evan Baer <evan@evanbaer.com>
2024-09-02 13:46:52 -0400

    Update banner

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -95,7 +95,7 @@ export default function Home({ isDemo, code }: Props) {
             <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
               <circle cx={1} cy={1} r={1} />
             </svg>
-            Auctions are back!
+            Labor Day special: place a bid and get 3 hours free hashrate!
           </a>
         </p>
       </div>
