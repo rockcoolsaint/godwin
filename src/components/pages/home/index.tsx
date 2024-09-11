6661a713aada80fba6e042b73a0fb21ac8d2093f
Evan Baer <evan@evanbaer.com>
2024-09-11 17:59:32 -0400

    disable direct sale

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -95,7 +95,7 @@ export default function Home({ isDemo, code }: Props) {
             <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
               <circle cx={1} cy={1} r={1} />
             </svg>
-           New auctions posted each week! 
+           Direct sale hashrate is temporarily offline
           </a>
         </p>
       </div>
