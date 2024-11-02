bfb609d723e385c9cc9f03bd3a1af036fce93341
Evan Baer <evan@evanbaer.com>
2024-11-02 13:14:17 -0400

    fix padding

--- src/components/pages/home/UpcomingAuctions/index.tsx
+++ src/components/pages/home/UpcomingAuctions/index.tsx
@@ -1,16 +1,16 @@
-import { Auction, AuctionOfTheDayResponse } from 'src/api/auction/types'
-import AuctionSchedule from 'src/components/pages/home/AuctionSchedule'
-import Link from 'src/components/shared/Link'
+import { Auction, AuctionOfTheDayResponse } from 'src/api/auction/types';
+import AuctionSchedule from 'src/components/pages/home/AuctionSchedule';
+import Link from 'src/components/shared/Link';
 
 interface AuctionOfTheDay {
-  auction: AuctionOfTheDayResponse
-  auctionsData: Auction[]
-  children?: React.ReactNode
+  auction: AuctionOfTheDayResponse;
+  auctionsData: Auction[];
+  children?: React.ReactNode;
 }
 
 function SellerBox({ name, capacity, desc }: { name: string, capacity: string, desc: string }) {
   return (
-    <div className="shadow-level-2 w-64 rounded-3xl border bg-white px-3 py-6 shadow-md flex flex-col justify-between h-full">
+    <div className="w-64 h-full rounded-3xl border bg-white px-3 py-6 shadow-lg flex flex-col justify-between">
       <div className="flex-grow">
         <h2 className="mb-4 text-xl font-semibold">{name}</h2>
         <p className="mb-4 text-medium text-dark-100">{desc}</p>
@@ -23,19 +23,21 @@ function SellerBox({ name, capacity, desc }: { name: string, capacity: string, d
         View auctions
       </Link>
     </div>
-  )
+  );
 }
 
 export default function UpcomingAuctions({ auctionsData, children }: AuctionOfTheDay) {
   return (
     <>
       <div className="mx-auto flex flex-col items-center justify-center overflow-auto">
-        <h1 className="mt-10 w-10/12 text-center font-chakra text-4xl text-navy lg:w-full lg:text-5xl 2xl:text-7xl">Auctions</h1>
+        <h1 className="mt-10 w-10/12 text-center font-chakra text-4xl text-navy lg:w-full lg:text-5xl 2xl:text-7xl">
+          Auctions
+        </h1>
 
         <AuctionSchedule auctionsData={auctionsData} showTitle={false} />
 
         {/* Seller Boxes */}
-        <div className="mt-10 grid grid-cols-1 gap-6 lg:grid-cols-3">
+        <div className="mt-10 mb-10 p-6 grid grid-cols-1 gap-8 lg:grid-cols-3">
           <SellerBox name="Baer Corp" capacity="5 PH/s" desc="Connecting small-scale bitcoin miners since 2022." />
           <SellerBox name="Midwest Mining" capacity="10 PH/s" desc="We're a local stranded natural gas mining farm in the Midwest." />
           <SellerBox name="Mining around the world" capacity="varies" desc="New mining farms on Rigly" />
@@ -44,5 +46,5 @@ export default function UpcomingAuctions({ auctionsData, children }: AuctionOfTh
       {/* SUPPORT */}
       {children}
     </>
-  )
+  );
 }
