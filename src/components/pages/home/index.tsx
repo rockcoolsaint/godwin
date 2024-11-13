a96adfb115088b61fec5e42ddf9663944b01c8d9
Evan Baer <evan@evanbaer.com>
2024-11-13 05:31:58 -0500

    edit external link to blockdyor review, also show Support section when not logged in

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -131,7 +131,7 @@ export default function Home({ isDemo, code }: Props) {
           <section className="auction-of-the-day-gradient flex w-full flex-col items-center justify-center px-4 pb-20 md:px-0">
             <h1 className="mt-20 font-chakra text-4xl text-navy lg:text-5xl 2xl:text-7xl">Support</h1>
             <p className="mt-2 text-center text-xs text-navy md:w-6/12 md:text-xl lg:w-7/12 lg:text-3xl 2xl:w-6/12">
-              Whether you&apos;re brand new to mining, or a seasoned bitcoiner, our support team is here to answer your questions
+              Have a question? Ask us via the chat window
             </p>
 
             <div className="mx-auto mt-14 flex flex-col justify-center md:items-center md:px-10 lg:flex-row xl:px-20">
@@ -154,14 +154,14 @@ export default function Home({ isDemo, code }: Props) {
               </div>
 
               <div className="mb-12 flex w-full flex-col items-start rounded-xl border border-gray-400 bg-white p-8 sm:mx-20 sm:min-h-[320px] md:mx-0 md:my-8 md:w-8/12 lg:mx-8 lg:my-0 xl:w-4/12 xl:px-8 2xl:w-3/12 2xl:px-12">
-                <h2 className="mb-5 w-10/12 text-xl font-semibold text-navy xl:text-3xl">How to bid on auctions</h2>
+                <h2 className="mb-5 w-10/12 text-xl font-semibold text-navy xl:text-3xl">Rigly Review 2024 by blockdyor</h2>
                 <p className="mb-5 text-lg text-black xl:text-xl">
-                  The Rigly experience is quick and seamless, giving you the opportunity to start mining right away.
+                  Read this in-depth tour of how to start mining and bid on Rigly.
                 </p>
 
                 <div className="">
                   <Link
-                    href="https://blog.rigly.io/how-to-bid-on-hashrate/"
+                    href="https://blockdyor.com/rigly-review"
                     className="flex rounded-lg bg-navy px-8 py-4 font-epilogue text-lg text-white hover:opacity-90"
                     target="_blank"
                   >
@@ -266,6 +266,65 @@ export default function Home({ isDemo, code }: Props) {
                     id="seller-info"
                   >
                     Learn more
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="auction-of-the-day-gradient flex w-full flex-col items-center justify-center px-4 pb-20 md:px-0">
+            <h1 className="mt-20 font-chakra text-4xl text-navy lg:text-5xl 2xl:text-7xl">Support</h1>
+            <p className="mt-2 text-center text-xs text-navy md:w-6/12 md:text-xl lg:w-7/12 lg:text-3xl 2xl:w-6/12">
+              Have a question? Ask us via the chat window
+            </p>
+
+            <div className="mx-auto mt-14 flex flex-col justify-center md:items-center md:px-10 lg:flex-row xl:px-20">
+              <div className="mb-12 flex w-full flex-col items-start rounded-xl border border-gray-400 bg-white p-8 sm:min-h-[320px] md:mb-0 md:w-8/12 xl:w-4/12 xl:px-8 2xl:w-3/12 2xl:px-12">
+                <div className="">
+                  <h2 className="mb-5 w-10/12 text-xl font-semibold text-navy xl:text-3xl">Learn how Bitcoin mining works</h2>
+                  <p className="mb-5 text-lg text-black xl:text-xl">
+                    There are lots of reasons why someone chooses to mine bitcoin. Read more in this essay from Braiins.
+                  </p>
+                </div>
+                <div className="">
+                  <Link
+                    href="https://braiins.com/blog/why-mine-bitcoin-braiins-mining"
+                    className="flex rounded-lg bg-navy px-8 py-4 font-epilogue text-lg text-white hover:opacity-90"
+                    target="_blank"
+                  >
+                    Read the article
+                  </Link>
+                </div>
+              </div>
+
+              <div className="mb-12 flex w-full flex-col items-start rounded-xl border border-gray-400 bg-white p-8 sm:mx-20 sm:min-h-[320px] md:mx-0 md:my-8 md:w-8/12 lg:mx-8 lg:my-0 xl:w-4/12 xl:px-8 2xl:w-3/12 2xl:px-12">
+                <h2 className="mb-5 w-10/12 text-xl font-semibold text-navy xl:text-3xl">Rigly Review 2024 by blockdyor</h2>
+                <p className="mb-5 text-lg text-black xl:text-xl">
+                  Read this in-depth tour of how to start mining and bid on Rigly.
+                </p>
+
+                <div className="">
+                  <Link
+                    href="https://blockdyor.com/rigly-review"
+                    className="flex rounded-lg bg-navy px-8 py-4 font-epilogue text-lg text-white hover:opacity-90"
+                    target="_blank"
+                  >
+                    Read the article
+                  </Link>
+                </div>
+              </div>
+
+              <div className="mb-12 flex w-full flex-col items-start rounded-xl border border-gray-400 bg-white p-8 sm:min-h-[320px] md:mb-0 md:w-8/12 md:px-6 xl:w-4/12 xl:px-8 2xl:w-3/12 2xl:px-16">
+                <div className="">
+                  <h2 className="mb-5 w-10/12 text-xl font-semibold text-navy xl:text-3xl">Want to sell your hashrate?</h2>
+                  <p className="mb-5 text-lg text-black xl:text-xl">Earn more for your hashrate on the Rigly marketplace.</p>
+                </div>
+                <div className="">
+                  <Link
+                    href="#"
+                    className="flex rounded-lg bg-navy px-8 py-4 font-epilogue text-lg text-white hover:opacity-90"
+                    target="_blank"
+                  >
+                    Read the article
                   </Link>
                 </div>
               </div>
