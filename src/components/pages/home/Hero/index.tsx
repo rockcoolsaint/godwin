d7ac5e4937ae861ee576777c6eb166596c4d5676
Evan Baer <evan@evanbaer.com>
2024-11-14 14:02:13 -0500

    adjust subheader

--- src/components/pages/home/Hero/index.tsx
+++ src/components/pages/home/Hero/index.tsx
@@ -4,11 +4,11 @@ const Hero = ({ children }: PropsWithChildren) => {
   return (
     <section className="mx-4 flex w-full flex-col items-center justify-center md:mb-12 lg:mb-0 lg:w-3/5">
       <div className="w-10/12 bg-gradient-to-r from-[#5C3FAF] to-[#316AEF] bg-clip-text text-center font-chakra text-4xl font-extrabold text-transparent sm:mb-2 sm:w-4/12 sm:pb-4 md:w-6/12 md:border-green-500 md:text-center md:text-5xl lg:w-8/12 lg:text-center xl:w-10/12 xl:text-8xl 2xl:w-8/12">
-        We're not cloud mining.
+        We're not cloud mining,
       </div>
       <div className="flex w-11/12 flex-col flex-wrap sm:flex-row sm:items-center sm:justify-center md:w-8/12 lg:w-4/6 xl:w-5/6">
         <p className="mb-2 text-center font-epilogue text-sm font-normal text-black lg:text-xl xl:text-4xl 2xl:w-11/12">
-          Rigly is the first auction marketplace for bitcoin mining
+          we're a bitcoin mining marketplace.
         </p>
       </div>
       {children}
