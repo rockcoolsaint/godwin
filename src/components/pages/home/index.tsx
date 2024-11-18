4475cad32f73210f63a3b0d6c439575e3da28b2a
Evan Baer <evan@evanbaer.com>
2024-11-18 18:58:18 -0500

    fix link

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -197,7 +197,7 @@ export default function Home({ isDemo, code }: Props) {
               <>
                 <div className="mb-20 mt-6 flex w-full justify-center font-chakra font-bold">
                 <Link
-                  href="/learn/rigly/start-mining-now/intro"
+                  href="/learn/rigly/start-mining-now"
                   className="lg:h-15 flex w-11/12 items-center justify-center rounded-full bg-gradient px-5 py-4 text-lg text-white outline-none hover:bg-gradient-hover disabled:cursor-not-allowed disabled:bg-gradient-disabled lg:w-8/12 lg:text-2xl xl:w-4/12"
                 >
                   Start mining now
