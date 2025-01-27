1708d4f20f08e2786e10050224f1599c8dd20ce4
Evan Baer <evan@evanbaer.com>
2025-01-26 19:09:07 -0500

    edit twitter card summary

--- app/page.tsx
+++ app/page.tsx
@@ -22,14 +22,14 @@ export async function generateMetadata(): Promise<Metadata> {
       maximumScale: 1,
     },
     title: 'Rigly',
-    keywords: ['bitcoin', 'mining', 'hashrate', 'bitcoin mining', 'rigly'],
+    keywords: ['bitcoin', 'mining', 'hashrate', 'bitcoin mining', 'rigly', 'upendo'],
     openGraph: {
-      images: [{ url: 'https://cdn.shopify.com/s/files/1/0603/6648/7720/files/Rigly_1.png?v=1654535433', width: 800, height: 600 }],
-      title: 'Rigly - Your Bitcoin Mining Marketplace',
+      images: [{ url: 'https://i.postimg.cc/59HBs7Qy/upendo-logo-orange.png', width: 400, height:150 }],
+      title: 'Upendo - Bitcoin Mining Block Party',
       description:
-        "Rigly is your marketplace for bitcoin mining. Buy hashrate from miners around the world. All plans include Trustless Mining escrow to ensure hashrate delivery. Don't get rugged, get Rigly.",
-      url: 'https://rigly.io',
-      siteName: 'Rigly',
+       "Solo mine with bitcoiners. We mine until we find a block and split 3.125 btc.",
+      url: 'https://upendo.rigly.io',
+      siteName: 'upendo.rigly.io',
       type: 'website',
     },
     twitter: {
