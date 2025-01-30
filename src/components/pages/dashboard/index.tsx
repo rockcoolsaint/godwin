915cc6aa5ad91e049bbf2c91f52fd440fcc9ba00
Evan Baer <evan@evanbaer.com>
2025-01-29 20:59:40 -0500

    line break on bonus hashrate tooltip

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -210,12 +210,14 @@ export default function Dashboard() {
                   <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clipRule="evenodd" />
                 </svg>
                 <span className="invisible group-hover:visible absolute left-0 transform -translate-y-full -translate-x-1/2 mt-0 px-2 py-1 bg-gray-900 text-white text-sm rounded-md whitespace-nowrap">
-                  The party earns bonus hashrate for every new bidder and high auction bid
+                  The party earns bonus hashrate<br/>for every new bidder and high auction bid
                 </span>
               </span>
             </p>
           </div>
         </div>
+
+        
 
         {/* Daily Odds Section */}
         <div>
