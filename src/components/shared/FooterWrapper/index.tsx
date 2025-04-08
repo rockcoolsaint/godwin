83b2dcbca2f45e62b5547ec886c728e8814a8fee
Evan Baer <evan@evanbaer.com>
2025-04-08 16:24:11 -0400

    remove global referral box

--- src/components/shared/FooterWrapper/index.tsx
+++ src/components/shared/FooterWrapper/index.tsx
@@ -3,18 +3,12 @@
 
 import { useMobileScreen } from 'src/hooks/useIsMobile'
 import Footer from '../Footer'
-import GlobalReferralBox from '../GlobalReferralBox'
 
 const FooterWrapper = () => {
   const isMobile = useMobileScreen()
 
   return (
     <div className="w-full">
-      {isMobile && (
-        <div className="mb-4">
-          <GlobalReferralBox />
-        </div>
-      )}
       <Footer />
     </div>
   )
