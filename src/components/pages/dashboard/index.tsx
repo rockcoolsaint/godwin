8e927a94f650d15c6a3bd88fba8d50771c78217a
Evan Baer <evan@evanbaer.com>
2025-04-14 10:27:29 -0400

    update for 4/26

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -252,7 +252,7 @@ function getNextBlockPartyDate() {
   nextSaturday.setHours(0, 0, 0, 0)
   
   // Add another week if the next block party is two weeks out
-  nextSaturday.setDate(nextSaturday.getDate() + 14)
+  nextSaturday.setDate(nextSaturday.getDate() + 7)
   
   const followingSaturday = addDays(nextSaturday, 7)
   
